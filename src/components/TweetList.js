7c629d7232801c750cdafe1773d8562f10b27343
Gatukgl <gatukgl@gmail.com>
2017-03-17 10:34:07 +0700

    Be able to go to first page and prevent infinite-loop

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -2,13 +2,20 @@ import React, { PropTypes } from 'react'
 import Tweet from './Tweet'
 
 class TweetList extends React.Component {
+  PropTypes = {
+    tweets: PropTypes.arrayOf(PropTypes.object),
+  }
+
   componentDidMount() {
     const ownerUsername = this.props.ownerUsername || 'kaizerwing'
     this.props.fetchTweets(ownerUsername)
   }
 
-  PropTypes = {
-    tweets: PropTypes.arrayOf(PropTypes.object),
+  componentWillReceiveProps(nextProps) {
+    if (this.props.ownerUsername !== nextProps.ownerUsername) {
+      const ownerUsername = nextProps.ownerUsername || 'kaizerwing'
+      this.props.fetchTweets(ownerUsername)
+    }
   }
 
   render() {
